c5924dba0d506f0778accb05d75f2caa1d19ac13
Konrad Kühne <konrad.kuehne@lambdaforge.io>
2019-07-26 19:28:45 +0200

    add initial frontend

--- src/cljs/takomo/core.cljs
+++ src/cljs/takomo/core.cljs
@@ -1,6 +1,27 @@
-(ns takomo.core)
+(ns takomo.core
+  (:require [reagent.core :as r]
+            ["@material-ui/core" :refer [Button]]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def click-count (r/atom 0))
+
+(defn simple-component []
+  [:div
+   [:p "The state has a value: " @click-count]
+   [:> Button {:variant "contained" :color "primary" :on-click #(swap! click-count inc)} "Click!"]])
+
+
+(defn init []
+  (r/render
+    [simple-component]
+    (js/document.getElementById "root")))
+
+
+(comment
+
+  @click-count
+
+  (r/render
+    [simple-component]
+    (js/document.getElementById "root"))
+
+  )

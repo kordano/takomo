fe587d63fd47fe46811e4ea3f71dad531c8d1aa2
Konrad Kühne <konrad.kuehne@lambdaforge.io>
2019-10-09 18:16:23 +0200

    restructure models and pages

--- src/cljs/takomo/member.cljs
+++ src/cljs/takomo/member.cljs
@@ -1,5 +1,5 @@
-(ns takomo.members
-  (:require [ajax.core :refer [GET PUT DELETE]]
+(ns takomo.member
+  (:require [ajax.core :refer [GET PUT DELETE POST]]
             [takomo.components :refer [field]]
             [accountant.core :as acc]
             [cljs.reader :refer [read-string]]))
@@ -72,3 +72,28 @@
                          (dissoc input :password))
               :keywords? true}))}
       "Save"]]))
+
+(defn new-member-page [state]
+  [:div.container
+   [:h1.title "Create new member"]
+   [field state :new-member :text :firstname "Firstname" "e.g. Alice"]
+   [field state :new-member :text :lastname "Lastname" "e.g. Abernathy"]
+   [field state :new-member :email :email "Email" "e.g. alice@umbrella.corp"]
+   [field state :new-member :password :password "Password" "e.g. alicerocks!1"]
+   [:a.button.is-primary
+    {:on-click
+     (fn []
+       (POST "http://localhost:3000/api/members"
+           {:handler (fn []
+                       (swap! state update-in [:notifications] conj "Member created!")
+                       (swap! state assoc-in [:inputs :new-member] nil))
+            :error-handler #(js/alert (read-string (str %)))
+            :response-format :json
+            :format :json
+            :headers {"Authorization" (str "Token " (-> @state :credentials :token))}
+            :params {:firstname (-> @state :inputs :new-member :firstname)
+                     :lastname (-> @state :inputs :new-member :lastname)
+                     :email (-> @state :inputs :new-member :email)
+                     :password (-> @state :inputs :new-member :password)}
+            :keywords? true}))}
+    "Create"]])
